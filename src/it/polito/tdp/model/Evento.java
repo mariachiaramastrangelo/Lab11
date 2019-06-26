090ea70b0b5d04f4c027f93ec81f03713a1d907b
macbook <macbook@192.168.1.76>
2019-06-26 20:44:15 +0200

    Primissima impostazione

---
+++ src/it/polito/tdp/model/Evento.java
@@ -0,0 +1,13 @@
+package it.polito.tdp.model;
+
+public class Evento {
+	
+	private LocalTime time;
+	private int num_persone;
+	
+	
+	public enum TipoEvento{
+		
+	}
+
+}
---
+++ src/it/polito/tdp/model/Simulatore.java
@@ -0,0 +1,15 @@
+package it.polito.tdp.model;
+
+public class Simulatore {
+	
+	//stato del sistema
+	//coda prioritaria
+	//parametri fissi
+	
+	
+	//valori in output
+	private int numero_totale_clienti;
+	private int numero_clienti_soddisfatti;
+	private int numero_clienti_insoddisfatti;
+
+}
