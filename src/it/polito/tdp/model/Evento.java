ad8ad1800dfe39c74ce435f5c8964fe7a68527eb
macbook <macbook@mariachmacbook2.lan>
2019-07-09 00:13:16 +0200

    evento

--- src/it/polito/tdp/model/Evento.java
+++ src/it/polito/tdp/model/Evento.java
@@ -1,8 +1,12 @@
 package it.polito.tdp.model;
 
+import java.time.LocalTime;
+import java.util.Random;
+
 public class Evento {
 	
 	private LocalTime time;
+	Random rand = new Random();
 	private int num_persone;
 	
 	
